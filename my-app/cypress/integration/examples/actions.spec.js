ed75b3a500cc2d633f130e8c2611eafcd7929736
Prashant Kandathil <prashantgeorge36@gmail.com>
2021-03-22 18:54:53 -0700

    Initial commit

---
+++ my-app/cypress/integration/examples/actions.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+describe('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('.type() - type into a DOM element', () => {
+    // https://on.cypress.io/type
+    cy.contains('Edit src/App.js and save to reload.')
+  })
+})
